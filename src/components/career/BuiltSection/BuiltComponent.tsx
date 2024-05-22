e5ebc0614ee4a0c55b142e765cdf90e3ef92e653
Nobody-123 <dikhyantkrishnadalai@gmail.com>
2024-05-22 11:09:58 +0530

    Made built section mobile responsive

--- src/components/career/BuiltSection/BuiltComponent.tsx
+++ src/components/career/BuiltSection/BuiltComponent.tsx
@@ -15,22 +15,23 @@ const BuiltComponent: React.FC<IBuiltComponentProps> = ({
   className,
 }) => {
   return (
-    <div
-      className={cn("w-full h-[500px] relative", className)}
-      style={{
-        background: `url(${coverImageUrl})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      <div className="absolute w-full h-full gradient-bottom-left-top-right"></div>
-      <div className="absolute bottom-0 w-3/4 pl-12 pb-12 pt-2 pr-[15px]">
-        <h1 className="mb-2 font-din-light text-[40px] font-medium text-white leading-[51.2px]">
+    <div className={cn("w-full relative", className)}>
+      <div
+        className="w-full h-[40vh] md:h-[500px]"
+        style={{
+          background: `url(${coverImageUrl})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <div className="hidden md:block absolute top-0 w-full h-full gradient-bottom-left-top-right"></div>
+      <div className="relative md:absolute bottom-0 w-full bg-black md:bg-transparent md:w-3/4 pl-[15px] sm:pl-12 pb-4 sm:pb-12 pt-2 sm:pr-[15px]">
+        <h1 className="mb-2 sm:pt-8 md:pt-0 font-din-light text-[40px] font-medium text-white leading-[51.2px]">
           {text}
         </h1>
         <h6
-          className="text-base font-medium text-white leading-6 mb-2 text-left"
+          className="hidden sm:block text-base font-medium text-white leading-6 mb-2 text-left"
           dangerouslySetInnerHTML={{ __html: subText }}
         />
       </div>
--- src/components/career/BuiltSection/BuiltSection.tsx
+++ src/components/career/BuiltSection/BuiltSection.tsx
@@ -4,14 +4,14 @@ import BuiltComponent from "./BuiltComponent";
 
 const BuiltSection: React.FC<any> = () => {
   return (
-    <div className="flex flex-wrap px-[33px] max-w-[1600px] w-full mx-auto mt-24">
+    <div className="flex flex-wrap sm:px-[33px] max-w-[1600px] w-full mx-auto mt-12">
       {builtSectionData.map((data, index) => {
         return (
           <BuiltComponent
             coverImageUrl={data?.coverImageUrl}
             text={data?.text}
             subText={data?.subText}
-            className="basis-12/12 lg:basis-4/12"
+            className="basis-12/12 lg:basis-4/12 mt-12"
           />
         );
       })}
